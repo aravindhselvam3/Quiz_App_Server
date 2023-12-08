8df1c9ced7c725aaa1838fbe560dc16f45b6aa6d
aravindhselvam3 <130632860+aravindhselvam3@users.noreply.github.com>
2023-12-08 07:27:39 +0530

    quiz backend

---
+++ database/data.js
@@ -0,0 +1,54 @@
+export default [
+    {
+        id: 1,
+        question : "What of the following is used in React.js to increase performance?",
+        options : [
+            'Original DOM',
+            'Virtual DOM',
+            'Both',
+            'None of the above'
+        ]
+    },
+    {
+        id: 2,
+        question : "in React, Which of the following acts as the input of a class-based component?",
+        options : [
+            'Class',
+            'Factory',
+            'Render',
+            'Props',
+        ]
+    },
+    {
+        id: 3,
+        question : "What is a state in React?",
+        options : [
+            'A permanent storage',
+            'Internal storage of the component',
+            'External storage of the component',
+            'None of the Above'
+        ]
+    },
+    {
+        id: 4,
+        question : " What are the two ways to handle data in React?",
+        options : [
+            'State & Props',
+            'Services & components',
+            'State and Services',
+            'State and Component'
+        ]
+    },
+    {
+        id: 5,
+        question : "Does React.js create a VIRTUAL DOM in the memory?",
+        options : [
+            'TRUE',
+            'FALSE',
+            'Can be true or false',
+            'Cannot say'
+        ]
+    }
+];
+
+export const answers = [1, 3, 1, 0, 0];
